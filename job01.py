43f73e3ab1a321ec5ac6a9884938d4a489a6d2a2
dehlia-neiss <dehlia.neiss@laplateforme.io>
2025-02-03 13:24:10 +0100

    add job 2 done

---
+++ job01.py
@@ -0,0 +1,12 @@
+class opération:
+    def __init__(self, nombre1=0, nombre2=0):
+     self.nombre1 = nombre1
+     self.nombre2 = nombre2
+
+operation_instance = opération()
+
+print(operation_instance)
+
+
+
+    
